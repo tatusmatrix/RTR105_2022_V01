402d24d980ad0ff197720c1c06644ea72e6c7d3d
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-09-20 14:02:58 +0300

    Class_05

---
+++ special_variable_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+# https://www.tutorialspoint.com/unix/unix-special-variables.htm#
+
