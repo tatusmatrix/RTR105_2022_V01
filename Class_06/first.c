84204aefe0ec81fe5f28dafdba8f64e76eb33bb8
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-09-27 09:54:43 +0300

    Class_06

---
+++ Class_06/first.c
@@ -0,0 +1,23 @@
+// vienas rindas komentārs
+// vienas rindas komentārs
+// vienas rindas komentārs
+
+/*
+komentāra paragrāfs vai bloks
+1.
+2.
+3.
+...
+*/
+
+int main()// arī te var būt koemntārs
+ {
+ /*
+ ar komentāra nokomentēts koda fragments
+ if(...)
+  {
+  ...
+  }
+ */
+ return 0;
+ }
---
+++ Class_06/first.i
@@ -0,0 +1,19 @@
+# 1 "first.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "first.c"
+# 13 "first.c"
+int main()
+ {
+
+
+
+
+
+
+
+ return 0;
+ }
